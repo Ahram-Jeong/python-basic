b0ad387b8a9317a7be00eb4c7bcc8aaed05d707f
Ahram-Jeong <ahram1215@gmail.com>
2024-05-16 01:45:14 +0900

    20240516 - 제어문_for

---
+++ control/for.py
@@ -0,0 +1,8 @@
+# Ex1
+for waitingNo in range(6) : # 0 ~ 5까지의 범위
+    print(f"대기번호 : {waitingNo}")
+
+# Ex2
+starbucks = ["조승연", "황민현", "차은우", "송강"]
+for customer in starbucks :
+    print(f"{customer} 고객님, 커피가 준비되었습니다.")
