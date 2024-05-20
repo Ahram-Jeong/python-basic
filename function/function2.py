be5b7610b43a2e5ed9e9025fe3b11fe2d17b85f4
Ahram-Jeong <ahram1215@gmail.com>
2024-05-20 19:57:26 +0900

    20240520 - function_parameter_기본 값 설정2

--- function/function2.py
+++ function/function2.py
@@ -1,12 +1,16 @@
 def profile(name, age, main_lang) :
-    print(f"이름 : {name}\t나이 : {age}세\t주 사용 언어{main_lang}")
+    print(f"이름 : {name},\t나이 : {age}세,\t주 사용 언어 : {main_lang}")
 
-profile("조승연", 28, "자바")
-profile("황민현", 27, "파이썬")
+profile("조승연", 28, "Java")
+profile("황민현", 27, "Python")
+
+# 키워드 값으로 args 입력
+profile(age = 50, main_lang = "Kotlin", name = "이주연")
+profile(main_lang = "Javascript", name = "차은우", age = 42)
 
 # 매개변수에 기본 값 설정
 def profile2(name, age = 8, main_lang = "Python") :
-    print(f"이름 : {name}\t나이 : {age}세\t주 사용 언어{main_lang}")
+    print(f"이름 : {name},\t나이 : {age}세,\t주 사용 언어 : {main_lang}")
 
 profile2("김루이")
 profile2("김로미")
