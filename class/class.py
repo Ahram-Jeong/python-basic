1036efb4b74fcf1aeb27902ca83a35f279f6c7a6
Ahram-Jeong <ahram1215@gmail.com>
2024-05-24 22:34:15 +0900

    20240524 - class_멤버 변수

--- class/class.py
+++ class/class.py
@@ -10,3 +10,16 @@ class Unit :
 marine1 = Unit("마린", 40, 5)
 marine2 = Unit("마린", 40, 5)
 tank = Unit("탱크", 150, 35)
+
+# Wraith : 공중 유닛, 비행기 + 클로킹
+wraith1 = Unit("레이스", 80, 5)
+print(f"유닛 이름 : {wraith1.name}, 공격력 : {wraith1.damage}")
+
+wraith2 = Unit("빼앗은 레이스", 80, 5)
+# 파이썬은 클래스 외부에서 원하는 변수를 확장할 수 있음
+# 확장된 변수는 확장을 한 객체에서만! 적용 됨
+wraith2.clocking = True
+
+if wraith2.clocking == True :
+    print(f"현재 {wraith2.name}는 클로킹 상태입니다.")
+
