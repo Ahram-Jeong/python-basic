e0834c5087a282f5a0ca4663ca2f308ba824b36a
Ahram-Jeong <ahram1215@gmail.com>
2024-07-26 22:01:03 +0900

    20240726 - operators_and, or

---
+++ basic/operators01.py
@@ -0,0 +1,14 @@
+# comparison operator
+record = "woodz"
+match = "woodz"
+print(record == match) # True
+
+# logical operators
+# and, or
+print(1 > 2 and 2 == 2) # False
+print(3 > 2 and 2 == 2) # True
+print(1 > 2 or 2 == 2) # True
+
+user_input = "mypassword"
+stored_password = "mypassword"
+print(user_input == stored_password) # True
