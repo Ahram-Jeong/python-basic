77a83549c51634d10c8f8b997b1e1d1e463fcbaf
Ahram-Jeong <ahram1215@gmail.com>
2024-07-30 15:15:08 +0900

    20240730 - module, package

---
+++ advanced/mymodule.py
@@ -0,0 +1,9 @@
+def useful_func():
+    print("Using the useful_func!")
+
+class UsefulClass():
+    def __init__(self, msg):
+        self.msg = msg
+
+    def report(self):
+        print(self.msg)
---
+++ advanced/myprogram.py
@@ -0,0 +1,9 @@
+from mymodule import useful_func, UsefulClass
+from mypackage.mysubmodule import my_sub_func
+
+useful_func() # Using the useful_func!
+
+myinstance = UsefulClass("Bonjour!")
+myinstance.report()
+
+my_sub_func() # Using a function from mysubmodule.py!
